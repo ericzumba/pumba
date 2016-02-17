7ce154e72880c5bba59be425938a921580ad765d
Eric Torti <eric.torti@vivareal.com>
2016-02-17 18:09:52 -0200

    ignoring prod.json

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 require('./lib/env')('prod', function(env){
   var solr = require('./lib/solr')(env.solr)
   var files = require('./lib/files')(env.files)
-
-  var slug = require('./lib/slug')()
   var gts = require('./lib/google-to-solr')()
 
   files.each(files.read(function(file) {
-    var doc = gts.convert(file)
-    doc.id = slug.id(doc.comps)
-    delete doc.comps
-    solr.add(doc, console.log)
+    solr.add(gts.convert(file), console.log)
   }))
 })
