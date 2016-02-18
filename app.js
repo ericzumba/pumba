491172c85c6461d77d7bf99e5c1e1e4238b0c18d
Eric Torti <eric.torti@vivareal.com>
2016-02-18 17:17:11 -0200

    moving converter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('./lib/env')(process.argv[2], function(env){
+require('./lib/env')(process.argv[2], function(env) {
   var solr = require('./lib/solr')(env.solr)
   var files = require('./lib/files')(env.files)
-  var gts = require('./lib/google-to-solr')()
+  var gts = require('./lib/converters/google-to-solr')()
 
   files.each(files.read(function(file) {
     solr.add(gts.convert(file), console.log)
